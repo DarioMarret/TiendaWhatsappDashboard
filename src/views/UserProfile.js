7ea4af8265a715c5ccd310280a4890909e2af710
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2021-04-01 14:16:08 -0500

    dash

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 // react-bootstrap components
 import {
@@ -12,8 +12,96 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import axios from "axios";
 
 function User() {
+
+  const [datosEmpresa, setdatosEmpresa] = useState({
+    empresa:'',
+    email:'',
+    whatsapp:'',
+    nombre:'',
+    apellido:'',
+    direccion:'',
+    ciudad:'',
+    provincia:'',
+    stado:''
+  })
+  const handleState=(e)=>{
+    setdatosEmpresa({
+      ...datosEmpresa,
+      [e.target.name]:e.target.value
+    })
+  }
+  const handleEmpresa=async()=>{
+    const {data} = await axios.get("http://localhost:3002/datos/empresa")
+    if (data){
+      console.log(data)
+      setdatosEmpresa(data)
+    }
+  }
+  const GrabarEmpresa=async(e)=>{
+    e.preventDefault();
+    const { empresa,email,whatsapp,nombre,apellido,direccion,ciudad,provincia } = datosEmpresa
+    if(empresa === "" || email === "" || whatsapp === "" || nombre === ""
+      || apellido === "" || direccion === "" || ciudad === "" || provincia === ""){
+      alert("Llenar todo los campos")
+    }else{
+      const {data} = await axios.post("http://localhost:3002/datos/empresa",{datosEmpresa})
+      if(data){
+        alert(data)
+      }
+    }
+  }
+
+  const handle=()=>{
+      let empresa = document.getElementById("empresa")
+       empresa.setAttribute("disabled", "")
+      let email = document.getElementById("email")
+      email.setAttribute("disabled", "")
+       let whatsapp = document.getElementById("whatsapp")
+      whatsapp.setAttribute("disabled", "")
+      let nombre = document.getElementById("nombre")
+      nombre.setAttribute("disabled", "")
+      let apellido = document.getElementById("apellido")
+      apellido.setAttribute("disabled", "")
+      let direccion = document.getElementById("direccion")
+      direccion.setAttribute("disabled", "")
+      let ciudad = document.getElementById("ciudad")
+      ciudad.setAttribute("disabled", "")
+      let provincia = document.getElementById("provincia")
+      provincia.setAttribute("disabled", "")
+  }
+
+  const [boo, setboo] = useState(true)
+  const Stado=async()=>{
+    let empresa = document.getElementById("empresa")
+       empresa.removeAttribute("disabled", "")
+      let email = document.getElementById("email")
+      email.removeAttribute("disabled", "")
+       let whatsapp = document.getElementById("whatsapp")
+      whatsapp.removeAttribute("disabled", "")
+      let nombre = document.getElementById("nombre")
+      nombre.removeAttribute("disabled", "")
+      let apellido = document.getElementById("apellido")
+      apellido.removeAttribute("disabled", "")
+      let direccion = document.getElementById("direccion")
+      direccion.removeAttribute("disabled", "")
+      let ciudad = document.getElementById("ciudad")
+      ciudad.removeAttribute("disabled", "")
+      let provincia = document.getElementById("provincia")
+      provincia.removeAttribute("disabled", "")
+    // const {data} = await axios.post("http://localhost:3002/stado",{boo})
+    // if(data){
+    //   handleEmpresa()
+    // }
+  }
+
+  useEffect(()=>{
+    handleEmpresa()
+    handle()
+  },[])
+
   return (
     <>
       <Container fluid>
@@ -30,9 +118,11 @@ function User() {
                       <Form.Group>
                         <label>Nombre de la Empresa</label>
                         <Form.Control
-                          defaultValue="Mr.Puffinice"
+                          defaultValue={datosEmpresa.empresa}
                           name="empresa"
+                          id="empresa"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -42,10 +132,12 @@ function User() {
                           Email address
                         </label>
                         <Form.Control
-                          defaultValue="Puffinice@htmail.com"
+                          defaultValue={datosEmpresa.email}
                           placeholder="Email"
                           name="email"
+                          id="email"
                           type="email"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -55,10 +147,11 @@ function User() {
                           Whatsapp conctanto
                         </label>
                         <Form.Control
-                          defaultValue=""
-                          placeholder="593993713942"
+                          defaultValue={datosEmpresa.whatsapp}
+                          id="whatsapp"
                           name="whatsapp"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -68,10 +161,12 @@ function User() {
                       <Form.Group>
                         <label>Nombre</label>
                         <Form.Control
-                          defaultValue="Fernando"
+                          defaultValue={datosEmpresa.nombre}
                           placeholder="Company"
+                          id="nombre"
                           name="nombre"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -79,10 +174,12 @@ function User() {
                       <Form.Group>
                         <label>Apellido</label>
                         <Form.Control
-                          defaultValue="Andrew"
+                          defaultValue={datosEmpresa.apellido}
                           placeholder="Last Name"
+                          id="apellido"
                           name="apellido"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -92,10 +189,12 @@ function User() {
                       <Form.Group>
                         <label>Direccion</label>
                         <Form.Control
-                          defaultValue="Santa-Elena / Salinas"
+                          defaultValue={datosEmpresa.direccion}
                           placeholder="Home Address"
                           name="direccion"
+                          id="direccion"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -105,9 +204,12 @@ function User() {
                       <Form.Group>
                         <label>Ciudad</label>
                         <Form.Control
-                          defaultValue="Mike"
+                          defaultValue={datosEmpresa.ciudad}
                           placeholder="City"
+                          name="ciudad"
+                          id="ciudad"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -115,26 +217,39 @@ function User() {
                       <Form.Group>
                         <label>Provincia</label>
                         <Form.Control
-                          defaultValue="Andrew"
+                          defaultValue={datosEmpresa.provincia}
                           placeholder="Country"
+                          name="provincia"
+                          id="provincia"
                           type="text"
+                          onChange={(e)=>handleState(e)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
+                  <div className="d-flex justify-content-between">
                   <Button
                     className="btn-fill pull-right"
-                    type="submit"
                     variant="info"
+                    onClick={(e)=>GrabarEmpresa(e)}
                   >
                     Guardar Datos
                   </Button>
+                  <Button
+                    className="btn btn-dark"
+                    variant="dark"
+                    onClick={()=>Stado()}
+                  >
+                    Show
+                  </Button>
+                  </div>
                   <div className="clearfix"></div>
                 </Form>
               </Card.Body>
             </Card>
           </Col>
           {/*//img presentacion*/}
+          
           <Col md="4">
             <Card className="card-user">
               <div className="card-image">
@@ -148,47 +263,22 @@ function User() {
               </div>
               <Card.Body>
                 <div className="author">
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
                     <img
                       alt="..."
                       className="avatar border-gray"
                       src={require("assets/img/faces/face-3.jpg").default}
                     ></img>
-                    <h5 className="title">Mike Andrew</h5>
-                  </a>
-                  <p className="description">michael24</p>
                 </div>
-                <p className="description text-center">
-                  "Lamborghini Mercy <br></br>
-                  Your chick she so thirsty <br></br>
-                  I'm in that two seat Lambo"
-                </p>
               </Card.Body>
+              <input type="file" name="logo" id="logo" className="form-control" />
               <hr></hr>
               <div className="button-container mr-auto ml-auto">
                 <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
+                  className="btn btn-info"
                   onClick={(e) => e.preventDefault()}
-                  variant="link"
+                  variant="info"
                 >
-                  <i className="fab fa-facebook-square"></i>
-                </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-twitter"></i>
-                </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-google-plus-square"></i>
+                  Subir Logo
                 </Button>
               </div>
             </Card>
